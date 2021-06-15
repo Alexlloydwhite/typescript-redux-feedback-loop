01dfb3afa2d56f99e2f7b5d7c4c6dc2d4caaff4b
Alex White <alexlloydwhite@gmail.com>
2021-06-15 17:17:27 -0500

    installing dependencies

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import { App } from './App';
+import { App } from './components/App/App';
 import reportWebVitals from './reportWebVitals';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
 
 ReactDOM.render(
   <React.StrictMode>
